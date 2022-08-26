36a8cc6b4ca505bb44d4b449e87bbf01f7a995e8
mckunda <davenolt.sh@gmail.com>
2022-08-26 20:27:20 +0300

    feat: implement ConfigurationProvider for k8s config maps

---
+++ KubernetesClient.Extensions.Configuration/KubernetesConfigMapConfigurationProvider.cs
@@ -0,0 +1,181 @@
+﻿using k8s;
+using k8s.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace KubernetesClient.Extensions.Configuration;
+
+public class KubernetesConfigMapConfigurationProvider : ConfigurationProvider, IDisposable
+{
+    private readonly Kubernetes _kubernetesClient;
+    private readonly KubernetesClientConfiguration _clientConfiguration;
+
+    private IDisposable? _configMapWatcher;
+    private bool _disposed = false;
+    private readonly object _watcherLock = new();
+
+    /// <summary>
+    /// The source settings for this provider.
+    /// </summary>
+    public KubernetesConfigMapConfigurationSource Source { get; }
+
+    public KubernetesConfigMapConfigurationProvider(KubernetesConfigMapConfigurationSource source)
+    {
+        Source = source ?? throw new ArgumentNullException(nameof(source));
+        
+        _clientConfiguration = KubernetesClientConfiguration.InClusterConfig();
+        _kubernetesClient = new Kubernetes(_clientConfiguration);
+
+        if (Source.ReloadOnChange)
+        {
+            StartConfigMapWatcher();
+        }
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        lock (_watcherLock)
+        {
+            _disposed = true;
+            _configMapWatcher?.Dispose();
+        }
+    }
+
+    private void StartConfigMapWatcher()
+    {
+        lock (_watcherLock)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+            
+            _configMapWatcher?.Dispose();
+            _configMapWatcher = CreateConfigMapWatcher();
+        }
+    }
+
+    private IDisposable CreateConfigMapWatcher()
+    {
+        var task = _kubernetesClient.CoreV1.ListNamespacedConfigMapWithHttpMessagesAsync("default", watch: true);
+        return task.Watch<V1ConfigMap, V1ConfigMapList>((type, item) =>
+        {
+            switch (type)
+            {
+                case WatchEventType.Added:
+                case WatchEventType.Modified:
+                case WatchEventType.Deleted:
+                    Load();
+                    break;
+                case WatchEventType.Error:
+                    StartConfigMapWatcher();
+                    break;
+            }
+        });
+    }
+
+    public override void Load()
+    {
+        var configs = _kubernetesClient.ListNamespacedConfigMap(
+            _clientConfiguration.Namespace,
+            labelSelector: $"app: {_clientConfiguration.Host}");
+
+        if (configs is null)
+        {
+            if (Source.Optional)
+            {
+                Data = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            }
+            else
+            {
+                throw new Exception(); // TODO: Concrete exception
+            }
+
+            OnReload();
+            return;
+        }
+
+        Data = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        foreach (var configMap in configs.Items)
+        {
+            if (configMap is null || configMap.Data.Count == 0)
+            {
+                continue;
+            }
+
+            foreach (var kvp in configMap.Data)
+            {
+                var newKey = $"{configMap.Metadata.Name}:{kvp.Key.Replace('.', ':')}";
+                Data.Add(newKey, kvp.Value);
+            }
+
+            if (configMap.BinaryData.Count == 0)
+            {
+                continue;
+            }
+
+            foreach (var kvp in configMap.BinaryData)
+            {
+                var newKey = kvp.Key;
+
+                if (kvp.Key.EndsWith('.'))
+                {
+                    newKey = newKey.Replace(".", "");
+                }
+
+                if (!kvp.Key.StartsWith('.'))
+                {
+                    newKey = $"{configMap.Metadata.Name}:{newKey}";
+                }
+
+                Data.Add(newKey, Convert.ToBase64String(kvp.Value));
+            }
+        }
+
+        OnReload();
+    }
+
+    /// <summary>
+    /// Converts a key from data field of a Kubernetes Config Map to a form suitable for
+    /// <see cref="ConfigurationProvider"/>
+    /// </summary>
+    /// <param name="key">Original key from Kubernetes Config Map.</param>
+    /// <param name="prefix">The name of the Kubernetes Config Map.</param>
+    /// <returns>
+    /// string representation of <see cref="ConfigurationProvider"/>-compatible
+    /// key for <see cref="ConfigurationProvider.Data"/>
+    /// </returns>
+    /// <remarks>
+    /// All non-alphanumeric character are deleted from the resulting string. Additionally, all digits are skipped
+    /// until the first occurrence of an alphabetical character.<br/>
+    /// If the original key starts with ".", the prefix won't be added.<br/>
+    /// If the original key ends with ".", then all dots of the original key will be deleted before processing.
+    /// </remarks>
+    private static string ConvertKey(string key, string prefix)
+    {
+        var newKey = key;
+
+        if (key.EndsWith('.'))
+        {
+            newKey = newKey.Replace(".", "");
+        }
+
+        if (!key.StartsWith('.'))
+        {
+            newKey = $"{prefix}:{newKey}";
+        }
+
+        return new string(newKey
+            .Where(c => char.IsLetterOrDigit(c) || c == ':')
+            .SkipWhile(c => !char.IsLetter(c))
+            .ToArray());
+    }
+
+    /// <summary>
+    /// Generates a string representing this provider name and relevant details.
+    /// </summary>
+    /// <returns> The configuration name. </returns>
+    public override string ToString()
+        => $"{GetType().Name} ({(Source.Optional ? "Optional" : "Required")})";
+}
---
+++ KubernetesClient.Extensions.Configuration/KubernetesConfigMapConfigurationSource.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace KubernetesClient.Extensions.Configuration;
+
+public class KubernetesConfigMapConfigurationSource : IConfigurationSource
+{
+    /// <summary>
+    /// Determines if loading the file is optional.
+    /// </summary>
+    public bool Optional { get; set; } = true;
+
+    /// <summary>
+    /// Determines whether the source will be loaded if the underlying file changes.
+    /// </summary>
+    public bool ReloadOnChange { get; set; } = true;
+
+    /// <summary>
+    /// Number of milliseconds that reload will wait before calling Load. This helps
+    /// avoid triggering reload before a file is completely written. Default is 250.
+    /// </summary>
+    public int ReloadDelay { get; set; } = 250;
+
+
+    /// <summary>
+    /// Builds the <see cref="IConfigurationProvider"/> for this source.
+    /// </summary>
+    /// <param name="builder">The <see cref="IConfigurationBuilder"/>.</param>
+    /// <returns>A <see cref="IConfigurationProvider"/></returns>
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        return new KubernetesConfigMapConfigurationProvider(this);
+    }
+}
